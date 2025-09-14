6113bee801e05c3d7a28d88cd5aafbec0695b758
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2025-09-14 10:02:13 +0200

    feat(ui): add Spinner component and integrate into CoinDetailsPage and HomPage
    - Created reusable Spinner component using BarLoader from react-spinners
    - Added Spinner to CoinDetailsPage and HomePage to display during loading state

--- src/pages/coin-details.jsx
+++ src/pages/coin-details.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router';
+import Spinner from '../components/Spinner';
 
 const API_URL = import.meta.env.VITE_COIN_API_URL;
 
@@ -41,7 +42,7 @@ const CoinDetailsPage = () => {
         {coin ? `${coin.name} (${coin.symbol.toUpperCase()})` : 'Coin Details'}
       </h1>
 
-      {loading && <p>Loading...</p>}
+      {loading && <Spinner />}
       {error && <div className='error'>❌ {error}</div>}
 
       {!loading && !error && (
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,6 +2,7 @@ import CoinCard from '../components/CoinCard';
 import LimitSelector from '../components/LimitSelector';
 import FilterInput from '../components/FilterInput';
 import SortSelector from '../components/SortSelector';
+import Spinner from '../components/Spinner';
 
 const HomePage = ({
   coins,
@@ -49,7 +50,7 @@ const HomePage = ({
   return (
     <div>
       <h1>💰 ₵rypto</h1>
-      {loading && <p>Loading...</p>}
+      {loading && <Spinner color='white' />}
       {error && <div className='error'>{error}</div>}
 
       {/* Filters */}
