c00e870556a3ac3daeeabe047cc9e35511de4fda
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2025-09-14 22:12:24 +0200

    ui(search): add placeholder text to search input

--- src/components/FilterInput.jsx
+++ src/components/FilterInput.jsx
@@ -3,6 +3,7 @@ function FilterInput({ filter, onFilterChange }) {
     <div className='filter'>
       <input
         type='text'
+        placeholder='Search by name or symbol (e.g. BTC, Ethereum)'
         value={filter}
         onChange={(e) => onFilterChange(e.target.value)}
       />
